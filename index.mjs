051bbb31bdb1b3761e0a8d6addc15c0eaf8a091d
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-05-21 18:20:52 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,9 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const ideaRoutes = require('./routes/ideas.routes');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import ideaRoutes from './routes/ideas.routes.js';
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
