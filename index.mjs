c835f139ad9110b54e413e388c4a7d436a2d3682
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-05-27 21:45:15 -0500

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -1,19 +1,19 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-import ideasRoutes from './routes/ideas.js';
-import emailRoutes from './routes/email.js';
+import dotenv from 'dotenv';
+import emailRoutes from './routes/email.routes.js';
+import ideasRoutes from './routes/ideas.routes.js';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/get-ideas', ideasRoutes);
-app.use('/api/send-email', emailRoutes);
+app.use('/api', emailRoutes);
+app.use('/api', ideasRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`✅ Server running on port ${port}`);
 });
